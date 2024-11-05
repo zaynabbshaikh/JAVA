11c1b6b5a8ce7a3acc2dcc4cb9de1c1429164b35
zaynabbshaikh <zainabshaikh7799@gmail.com>
2024-11-05 16:18:33 +0000

    dataypes done

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all the JS code as newer version
+
+// alert( 3 + 3 ) //we are using nodejs, not browser 
+// code readability should be high
+
+// null is an object
+
+// TYPES OF DATATYPES:
+    // number => 2 to power 53 
+    // bigint
+    // string => ""
+    // boolen => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+
+    // object
+
+    console.log(typeof undefined)
+    console.log(typeof null);
+    
