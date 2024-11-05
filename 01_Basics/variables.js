a351af844b566c70f5e9eb0f4bb90d66aca94f14
zaynabbshaikh <zainabshaikh7799@gmail.com>
2024-11-05 16:18:13 +0000

    var done

---
+++ 01_Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 125776
+let accountEmail = "zainabshaikh@google.com"
+var accountPassword = "123456"
+accountCity = "Seoul"
+
+// accountId = 2 not allowed
+
+/*Perfer not to use var 
+because of issue in block scope and functional scope */
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
