be8cd59fb47d843f13cc673ac5be6bf9d092ac87
zaynabbshaikh <zainabshaikh7799@gmail.com>
2024-11-20 13:41:26 +0000

    Datatypes done

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,15 @@
+//  Primitive 
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol
+// BigInt
+ const score = 100  
+ const scoreValue = 100.3  
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
